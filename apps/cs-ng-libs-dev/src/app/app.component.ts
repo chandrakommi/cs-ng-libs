8db4ae4ac0f9ca281f4b639f579dcc85026c30c8
chandrakommi <chandrasekhar.kommi@outlook.com>
2023-06-09 16:22:47 +0530

    Inital setup

---
+++ apps/cs-ng-libs-dev/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cs-ng-libs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cs-ng-libs-dev';
+}
